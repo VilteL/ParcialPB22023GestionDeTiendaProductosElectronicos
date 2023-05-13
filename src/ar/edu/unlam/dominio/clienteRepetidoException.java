1c54227fa6eb4d827bcee85ed589ee735b741604
Vilte Leonardo <lvilte151@alumno.unlam.edu.ar>
2023-05-12 22:06:17 -0300

    agregando clienteRepetidoException

---
+++ src/ar/edu/unlam/dominio/clienteRepetidoException.java
@@ -0,0 +1,13 @@
+package ar.edu.unlam.dominio;
+
+public class clienteRepetidoException extends Exception {
+	
+	public clienteRepetidoException(String msj) {
+		super(msj);
+	}
+	
+	public clienteRepetidoException() {
+	}
+	
+	
+}
